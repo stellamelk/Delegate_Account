721d8858605c6229638e5d033eede23c793e50f2
Stella Melkonyan <stella.melkonyan.benivo@outlook.com>
2021-12-03 23:08:51 +0400

    Add project files.

---
+++ Delegate1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class Account
+    {
+        int _sum; // variable keep money
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+                if (_del != null)
+                    _del($"{sum} withdrawn from the account");
+            }
+            else 
+            {
+                if (_del != null)
+                    _del("Not enough money in the account");
+               
+            }
+        }
+        public delegate void AccountStateHandler(string message);
+        AccountStateHandler _del;//create variable of delegate
+        //registration of delegate
+        public void RegisterHandler(AccountStateHandler del)
+        {
+            _del = del;
+        }
+    }
+}
---
+++ Delegate1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create account
+            Account account = new Account(200);
+
+            account.RegisterHandler(new Account.AccountStateHandler(Show_Message));
+            account.Withdraw(100);
+            account.Withdraw(150);
+            Console.ReadLine();
+        }
+        private static void Show_Message(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
